a7db22ffc5a2a328aee7088d66c9ad399938de12
AV_Aram <aram.vardanyan.07@bk.ru>
2025-08-02 09:12:11 +0400

    server optimization

--- script.js
+++ script.js
@@ -1,62 +1,44 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
+
+const TEMPLATE_PATH = path.join(__dirname, 'templates');
 
 const server = http.createServer((req, res) => {
+    let fileName = '';
+
     if (req.url === '/') {
-        fs.readFile('./templates/home.html', 'utf-8', (err, data) => {
-            if (err) {
-                res.write('<h1>Error loading page Home</h1>');
-                return res.end();
-            }
-            res.write(data);
-            res.end();
-        });
+        fileName = 'home.html';
     } else if (req.url === '/about') {
-        fs.readFile('./templates/about.html', 'utf-8', (err, data) => {
-            if (err) {
-                res.write('<h1>Error loading page About</h1>');
-                return res.end();
-            }
-            res.write(data);
-            res.end();
-        });
+        fileName = 'about.html';
     } else if (req.url === '/contact') {
-        fs.readFile('./templates/contact.html', 'utf-8', (err, data) => {
-            if (err) {
-                res.write('<h1>Error loading page Contact</h1>');
-                return res.end();
-            }
-            res.write(data);
-            res.end();
-        });
+        fileName = 'contact.html';
     } else if (req.url === '/services') {
-        fs.readFile('./templates/services.html', 'utf-8', (err, data) => {
-            if (err) {
-                res.write('<h1>Error loading page Services</h1>');
-                return res.end();
-            }
-            res.write(data);
-            res.end();
-        });
+        fileName = 'services.html';
     } else if (req.url === '/blog') {
-        fs.readFile('./templates/blog.html', 'utf-8', (err, data) => {
-            if (err) {
-                res.write('<h1>Error loading page Blog</h1>');
-                return res.end();
-            }
-            res.write(data);
-            res.end();
-        });
+        fileName = 'blog.html';
     } else {
         res.write('<h1>404 - Page not found</h1>');
-        res.end();
+        return res.end();
     }
+
+    const filePath = path.join(TEMPLATE_PATH, fileName);
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            res.write('<h1>Error loading the page</h1>');
+            return res.end();
+        }
+        res.write(data);
+        res.end();
+    });
 });
 
 server.listen(3000, (err) => {
     if (err) {
-        console.log('Error starting server:', err);
+        console.log(err);
     } else {
-        console.log('The server is running');
+        console.log('Server is running');
     }
 });
+
